69a5451ce7a2b89b802d489bb6cfd591114edcdf
Pim de Wit <dewit@dewit-macbookpro2.roam.corp.google.com>
2016-12-05 11:38:07 +0000

    Restart for 2017

--- app/scripts/config/cfg-assets.js
+++
@@ -1,7 +0,0 @@
-/**
- *  Routing/asset settings.
- */
-Pokemon.configAssets = {
-  MAPS: 'scripts/maps/',
-  WATERTILE: 'images/tiles/water/'
-};
--- app/scripts/config/cfg-renderer.js
+++
@@ -1,8 +0,0 @@
-/**
- *  Render settings.
- */
-Pokemon.configRenderer = {
-  BACKGROUND: 0xf3f4f5,
-  ANTIALIAS: true,
-  AUTORESIZE: true
-};
